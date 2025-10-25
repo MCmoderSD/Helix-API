90e60f49f6c37448604e5198d83a03208a042010
Seraphin Berger <mail@mcmodersd.de>
2025-10-25 07:00:41 +0200

    Updated Examples and README.md

--- README.md
+++ README.md
@@ -25,3 +25,68 @@ Add the dependency to your `pom.xml` file:
     <version>2.0.0</version>
 </dependency>
 ```
+
+### Example
+```java
+import com.fasterxml.jackson.databind.JsonNode;
+
+import de.MCmoderSD.helix.core.HelixHandler;
+import de.MCmoderSD.helix.enums.Scope;
+import de.MCmoderSD.helix.handler.ChannelHandler;
+import de.MCmoderSD.helix.handler.RoleHandler;
+import de.MCmoderSD.helix.handler.UserHandler;
+
+import de.MCmoderSD.json.JsonUtility;
+import de.MCmoderSD.server.core.Server;
+
+import java.io.IOException;
+
+import java.net.URISyntaxException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@SuppressWarnings("ALL")
+public class Main {
+
+    public static HelixHandler initHelix() {
+
+        // Load Config
+        JsonNode config;
+        try {
+            config = JsonUtility.getInstance().load("/config.json");
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Could not read config.json", e);
+        }
+
+        // Load Config
+        JsonNode applicationConfig = config.get("twitch").get("application");
+        JsonNode databaseConfig = config.get("database");
+        JsonNode serverConfig = config.get("server");
+
+        // Init Server
+        Server server = new Server(serverConfig);
+        server.start();
+
+        // Init HelixHandler
+        return new HelixHandler(applicationConfig, databaseConfig, server);
+    }
+
+    public static void main(String[] args) {
+
+        // Init HelixHandler
+        HelixHandler helixHandler = initHelix();
+
+        // Print Authorization URL
+        Scope[] scopes = new ArrayList<>(Arrays.asList(UserHandler.REQUIRED_SCOPES, ChannelHandler.REQUIRED_SCOPES, RoleHandler.REQUIRED_SCOPES))
+                .stream()
+                .flatMap(Stream::of)
+                .distinct()
+                .toArray(Scope[]::new);
+
+        String authURL = helixHandler.getAuthorizationUrl(scopes);
+        System.out.println("\n\nAuthorization URL: " + authURL + "\n");
+    }
+}
+```
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,144 +1,61 @@
 import com.fasterxml.jackson.databind.JsonNode;
 
-import de.MCmoderSD.helix.config.Configuration;
 import de.MCmoderSD.helix.core.HelixHandler;
 import de.MCmoderSD.helix.enums.Scope;
 import de.MCmoderSD.helix.handler.ChannelHandler;
 import de.MCmoderSD.helix.handler.RoleHandler;
 import de.MCmoderSD.helix.handler.UserHandler;
-import de.MCmoderSD.helix.objects.ChannelFollower;
 
 import de.MCmoderSD.json.JsonUtility;
-import de.MCmoderSD.server.Server;
-import de.MCmoderSD.sql.Driver;
+import de.MCmoderSD.server.core.Server;
 
 import java.io.IOException;
 
 import java.net.URISyntaxException;
 
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @SuppressWarnings("ALL")
 public class Main {
 
-    public static void main(String[] args) throws IOException, URISyntaxException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, InterruptedException, KeyManagementException {
+    public static HelixHandler initHelix() {
 
         // Load Config
-        JsonNode config = JsonUtility.loadJson("/config.json", false);
-        JsonNode jks = JsonUtility.loadJson("/server.json", false);
+        JsonNode config;
+        try {
+            config = JsonUtility.getInstance().load("/config.json");
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Could not read config.json", e);
+        }
 
-        // Configure Helix
-        Configuration.setClientId(config.get("clientId").asText());
-        Configuration.setClientSecret(config.get("clientSecret").asText());
+        // Load Config
+        JsonNode applicationConfig = config.get("twitch").get("application");
+        JsonNode databaseConfig = config.get("database");
+        JsonNode serverConfig = config.get("server");
 
-        // Configure Database
-        Configuration.setDatabaseType(Driver.DatabaseType.SQLITE);
-        Configuration.setDatabase("database.db");
-
-        // Initialize Server
-        Server server = new Server("localhost", 8000, null, jks, true);
+        // Init Server
+        Server server = new Server(serverConfig);
         server.start();
 
-        // Initialize HelixHandler
-        HelixHandler helixHandler = new HelixHandler(server);
-        System.out.println("Auth URL: " + helixHandler.getTokenHandler().getAuthorizationUrl(Scope.values()));
-
-        // Test API
-        Scanner scanner = new Scanner(System.in);
-        while (true) printChannelInfo(scanner.nextLine(), helixHandler.getUserHandler(), helixHandler.getChannelHandler());
+        // Init HelixHandler
+        return new HelixHandler(applicationConfig, databaseConfig, server);
     }
 
-    private static void printChannelInfo(String channel, UserHandler userHandler, ChannelHandler channelHandler) {
+    public static void main(String[] args) {
 
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
+        // Init HelixHandler
+        HelixHandler helixHandler = initHelix();
 
-        // Get Channel Information and print it
-        channelHandler.getChannelInformation(user.getId()).print();
-    }
+        // Print Authorization URL
+        Scope[] scopes = new ArrayList<>(Arrays.asList(UserHandler.REQUIRED_SCOPES, ChannelHandler.REQUIRED_SCOPES, RoleHandler.REQUIRED_SCOPES))
+                .stream()
+                .flatMap(Stream::of)
+                .distinct()
+                .toArray(Scope[]::new);
 
-    private static void printModerators(String channel, UserHandler userHandler, RoleHandler roleHandler) {
-
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
-
-        // Get Moderators
-        var moderators = roleHandler.getModerators(user.getId(), null);
-        System.out.println("\nModerators: " + moderators.size());
-        for (var moderator : moderators) System.out.println(String.format(" - %s (%d) - %s",
-                moderator.getDisplayName(),
-                moderator.getId(),
-                moderator.getBroadcasterType()
-        ));
-    }
-
-    private static void printEditors(String channel, UserHandler userHandler, RoleHandler roleHandler) {
-
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
-
-        // Get Editors
-        var editors = roleHandler.getEditors(user.getId());
-        System.out.println("\nEditors: " + editors.size());
-        for (var editor : editors) System.out.println(String.format(" - %s (%d) - %s",
-                editor.getDisplayName(),
-                editor.getId(),
-                editor.getBroadcasterType()
-        ));
-    }
-
-    private static void printVips(String channel, UserHandler userHandler, RoleHandler roleHandler) {
-
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
-
-        // Get Vips
-        var vips = roleHandler.getVIPs(user.getId(), null);
-        System.out.println("\nVIPs: " + vips.size());
-        for (var vip : vips) System.out.println(String.format(" - %s (%d) - %s",
-                vip.getDisplayName(),
-                vip.getId(),
-                vip.getBroadcasterType()
-        ));
-    }
-
-    private static void printSubscribers(String channel, UserHandler userHandler, RoleHandler roleHandler) {
-
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
-
-        // Get Subscriber
-        var subscriber = roleHandler.getSubscribers(user.getId(), null);
-        System.out.println("\nSubscriber: " + subscriber.size());
-        for (var sub : subscriber) sub.print();
-    }
-
-    private static void printFollowers(String channel, UserHandler userHandler, RoleHandler roleHandler) {
-
-        // Get User
-        var user = userHandler.getTwitchUser(channel);
-        System.out.println("User: " + user.getDisplayName() + " (" + user.getId() + ")");
-
-        // Get Follower
-        var followers = roleHandler.getFollowers(user.getId(), null);
-        var sortedFollowers = new ArrayList<>(followers);
-        sortedFollowers.sort((o1, o2) -> {
-            if (o1.getFollowedAt() == null || o2.getFollowedAt() == null) return 0;
-            return o1.getFollowedAt().compareTo(o2.getFollowedAt());
-        });
-        System.out.println("\nFollowers: " + followers.size());
-        sortedFollowers.forEach(ChannelFollower::print);
+        String authURL = helixHandler.getAuthorizationUrl(scopes);
+        System.out.println("\n\nAuthorization URL: " + authURL + "\n");
     }
 }
